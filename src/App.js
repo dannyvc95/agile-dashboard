36c178b1b9eb41d8964a6a175687b650f101a42f
AlfonsoKrad <65353103+AlfonsoKrad@users.noreply.github.com>
2021-08-12 18:39:15 -0500

    Framework init
    Add rsuite framework

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "rsuite";
+import "rsuite/dist/styles/rsuite-default.css";
+
 const App = () => {
-  return <>Moshi Moshi Alfonso Desu</>;
+  return (
+    <>
+      <Button appearance="ghost" style={{ marginLeft: "10%", marginTop: "5%" }}>
+        Moshi moshi Fer desu
+      </Button>
+    </>
+  );
 };
 
 export default App;
